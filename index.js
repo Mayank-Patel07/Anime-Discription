45da6925ceb8d24ce01d478d61ad9b9e5838e6d5
Mayank Patel <mahindp1111@gmail.com>
2023-06-20 08:34:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+nav = document.querySelector('.nav')
+nav2 = document.querySelector('.nav2')
+
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('h-nav');
+    nav2.classList.toggle('v-class')
+    
+}
+
+)
